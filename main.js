971e5cdcc8c616ade93a033fb0bbf77aef1a116a
01-webseiten-projekt-Lucasoler09 <e228154@edu.sbl.ch>
2025-11-17 09:16:02 +0000

    17.11 Elements

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hallo Welt!");
+let counter = 0
+
+funktion createNewElement(){
+    let newElement = document.createElement("p")
+
+counter = counter + 1
+
+    newElement.textContent = "Element " + counter
+
+    let mainElement = document.querySelector("main");
+    mainElement.appendChild(newElement);
+}
