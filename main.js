daa1810b4f00ff8d7210113d9e3d27d67043e46f
01-webseiten-projekt-Lucasoler09 <e228154@edu.sbl.ch>
2025-11-17 09:17:47 +0000

    17.11 element

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let counter = 0
+let counter = 0;
 
 funktion createNewElement(){
-    let newElement = document.createElement("p")
+    let newElement = document.createElement("p");
 
-counter = counter + 1
+counter = counter + 1;
 
-    newElement.textContent = "Element " + counter
+    newElement.textContent = "Element " + counter;
 
     let mainElement = document.querySelector("main");
     mainElement.appendChild(newElement);
